073611b7139789973f650a9f87e788d25433e758
victor.onwuhafua <victor.onwuhafua@ISW-GSD-VO>
2022-01-12 15:21:24 +0100

    Add project files.

---
+++ Models/DBCONTEXT.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using SIMPLE_LOGIN_SIGNUP_API.Entities;
+
+namespace SIMPLE_LOGIN_SIGNUP_API.Models
+{
+    public class DBCONTEXT : DbContext
+    {
+        public DBCONTEXT(DbContextOptions options) : base(options) { }
+        DbSet<User> users
+        {
+            get;
+            set;
+        }
+    }
+}
